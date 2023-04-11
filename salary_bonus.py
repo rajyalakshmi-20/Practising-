7d177251b4d6a9cf3f4e9a30d51cd515982d6635
rajyalakshmi-20 <130435632+rajyalakshmi-20@users.noreply.github.com>
2023-04-11 12:55:31 +0530

    Add files via upload

---
+++ salary_bonus.py
@@ -0,0 +1,11 @@
+salary = int(input("Enter your salary:"))
+
+service_yrs = int(input("Enter Years of service:"))
+
+if service_yrs > 5:
+
+ print("Yours salary(+Bonus) = ",salary + (salary)*5/100)
+
+else:
+
+ print("You are not eligible for bonus as you have less service years.")
