beb7af2532a1fcb9c9cb427938e825e0bc2f3ce6
rajyalakshmi-20 <130435632+rajyalakshmi-20@users.noreply.github.com>
2023-05-24 11:16:26 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,9 @@
+from Insertion_At_Start import *
+LinkedList()
+print("-----------------")
+from Insertion_At_Middle import *
+LinkedList()
+print("-----------------")
+from Insertion_At_End import *
+LinkedList()
+
